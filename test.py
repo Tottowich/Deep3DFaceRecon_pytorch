72f58faa48a30fc078c94f7c3f6f3dfde1af1472
Theodor Jonsson <tjo020311@gmail.com>
2022-08-17 14:30:59 +0200

    Initial Commit

--- test.py
+++ test.py
@@ -2,6 +2,7 @@
 """
 
 import os
+from turtle import left
 from options.test_options import TestOptions
 from data import create_dataset
 from models import create_model
@@ -13,21 +14,36 @@ from util.load_mats import load_lm3d
 import torch 
 from data.flist_dataset import default_flist_reader
 from scipy.io import loadmat, savemat
+import matplotlib.pyplot as plt
+from mtcnn import MTCNN
 
 def get_data_path(root='examples'):
     
     im_path = [os.path.join(root, i) for i in sorted(os.listdir(root)) if i.endswith('png') or i.endswith('jpg')]
-    lm_path = [i.replace('png', 'txt').replace('jpg', 'txt') for i in im_path]
-    lm_path = [os.path.join(i.replace(i.split(os.path.sep)[-1],''),'detections',i.split(os.path.sep)[-1]) for i in lm_path]
+    # lm_path = [i.replace('png', 'txt').replace('jpg', 'txt') for i in im_path]
+    # lm_path = [os.path.join(i.replace(i.split(os.path.sep)[-1],''),'detections',i.split(os.path.sep)[-1]) for i in lm_path]
 
-    return im_path, lm_path
+    return im_path#, lm_path
 
-def read_data(im_path, lm_path, lm3d_std, to_tensor=True):
+def read_data(im_path, lm3d_std, to_tensor=True):
     # to RGB 
     im = Image.open(im_path).convert('RGB')
+    detector = MTCNN()
+    faces = detector.detect_faces(np.array(im))
+    if len(faces) == 0:
+        return None, None
+    face = faces[0]
+    nose, mouth_right, right_eye, left_eye, mouth_left = face['keypoints'].values()
+    lm = np.array([nose,mouth_right,right_eye,left_eye,mouth_left],dtype=np.float32)
+    # print(lm.shape)
+    # exit()
+    # print(nose, mouth_right, right_eye, left_eye, mouth_left)
     W,H = im.size
-    lm = np.loadtxt(lm_path).astype(np.float32)
-    lm = lm.reshape([-1, 2])
+    # exit()
+    # lm2 = np.loadtxt(lm_path).astype(np.float32)
+    # lm2 = lm.reshape([-1, 2])
+    # print(f"{lm2.shape} vs {lm.shape}")
+
     lm[:, -1] = H - 1 - lm[:, -1]
     _, im, lm, _ = align_img(im, lm, lm3d_std)
     if to_tensor:
@@ -45,15 +61,18 @@ def main(rank, opt, name='examples'):
     model.eval()
     visualizer = MyVisualizer(opt)
 
-    im_path, lm_path = get_data_path(name)
+    im_path = get_data_path(name) #, lm_path
     lm3d_std = load_lm3d(opt.bfm_folder) 
 
     for i in range(len(im_path)):
         print(i, im_path[i])
         img_name = im_path[i].split(os.path.sep)[-1].replace('.png','').replace('.jpg','')
-        if not os.path.isfile(lm_path[i]):
-            continue
-        im_tensor, lm_tensor = read_data(im_path[i], lm_path[i], lm3d_std)
+        # if not os.path.isfile(lm_path[i]):
+        #     continue
+        im_tensor, lm_tensor = read_data(im_path[i], lm3d_std)
+        plt.imshow(im_tensor[0].permute(1,2,0).numpy())
+        plt.scatter(lm_tensor[0,:,0], lm_tensor[0,:,1])
+        plt.show()
         data = {
             'imgs': im_tensor,
             'lms': lm_tensor
@@ -63,8 +82,9 @@ def main(rank, opt, name='examples'):
         visuals = model.get_current_visuals()  # get image results
         visualizer.display_current_results(visuals, 0, opt.epoch, dataset=name.split(os.path.sep)[-1], 
             save_results=True, count=i, name=img_name, add_image=False)
-
-        model.save_mesh(os.path.join(visualizer.img_dir, name.split(os.path.sep)[-1], 'epoch_%s_%06d'%(opt.epoch, 0),img_name+'.obj')) # save reconstruction meshes
+        path = os.path.join(visualizer.img_dir, name.split(os.path.sep)[-1], 'epoch_%s_%06d'%(opt.epoch, 0),img_name+'.obj')
+        print(path)
+        model.save_mesh(path) # save reconstruction meshes
         model.save_coeff(os.path.join(visualizer.img_dir, name.split(os.path.sep)[-1], 'epoch_%s_%06d'%(opt.epoch, 0),img_name+'.mat')) # save predicted coefficients
 
 if __name__ == '__main__':
---
+++ test_vis.py
@@ -0,0 +1,91 @@
+"""This script shows an example of using the PyWavefront module."""
+import ctypes
+import os
+import sys
+
+sys.path.append('..')
+
+import pyglet
+from pyglet.gl import *
+
+from pywavefront import visualization
+from pyglet.window import key
+import pywavefront
+import cv2
+
+# Create absolute path from this module
+file_abspath = os.path.join(os.path.dirname(__file__), './checkpoints/FaceReconTorch/results/epoch_20_000000/badlight.obj')
+
+rotation_x = 0
+rotation_y = 0
+direction_x = 1
+direction_y = 1
+meshes = pywavefront.Wavefront(file_abspath)
+window = pyglet.window.Window(resizable=True)
+keys = pyglet.window.key.KeyStateHandler()
+lightfv = ctypes.c_float * 4
+
+
+@window.event
+def on_resize(width, height):
+    viewport_width, viewport_height = window.get_framebuffer_size()
+    glViewport(0, 0, viewport_width, viewport_height)
+
+    glMatrixMode(GL_PROJECTION)
+    glLoadIdentity()
+    gluPerspective(90., float(width)/height, 1., 100.)
+    glMatrixMode(GL_MODELVIEW)
+    return True
+
+
+@window.event
+def on_draw():
+    window.clear()
+    glLoadIdentity()
+
+    glLightfv(GL_LIGHT0, GL_POSITION, lightfv(-1.0, 1.0, 1.0, 0.0))
+    glEnable(GL_LIGHT0)
+
+    glTranslated(0.0, 0.0, -3.0)
+    # glRotatef(rotation, 0.0, 1.0, 0.0)
+    # glRotatef(-25.0, 1.0, 0.0, 0.0)
+    # glRotatef(45.0, 0.0, 0.0, 1.0)
+    glRotatef(rotation_x, 0.0, 1.0, 0.0)
+    glRotatef(rotation_y, 1.0, 0.0, 0.0)
+    glRotatef(0.0, 0.0, 0.0, 1.0)
+    glEnable(GL_LIGHTING)
+
+    visualization.draw(meshes)
+
+# @window.event
+# def on_key_press(symbol, modifiers):
+#     global rotation_x
+#     global rotation_y
+#     print(f"{symbol} {modifiers}")
+#     print(f"{key.UP} {key.DOWN} {key.LEFT} {key.RIGHT}")
+#     if symbol == key.UP:
+#         rotation_y += 1
+#     elif symbol == key.DOWN:
+#         rotation_y -= 1
+#     elif symbol == key.LEFT:
+#         rotation_x += 1
+#     elif symbol == key.RIGHT:
+#         rotation_x -= 1
+def update(dt):
+    global rotation_x
+    global rotation_y
+    global direction_x
+    global direction_y
+    rotation_x += direction_x*90*dt
+    #rotation_y += direction_y*90*dt
+    if abs(rotation_x) > 90.0:
+        direction_x = -direction_x
+    if abs(rotation_y) > 720.0:
+        direction_y = -direction_y
+        
+    
+    
+
+
+pyglet.clock.schedule(update)
+pyglet.app.run()
